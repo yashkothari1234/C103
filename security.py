3becb97dced965f5860c1eaf476b7234312b64a2
yashkothari1234 <68227727+yashkothari1234@users.noreply.github.com>
2021-04-13 16:55:21 +0530

    Add files via upload

---
+++ security.py
@@ -0,0 +1,17 @@
+import cv2
+import dropbox
+import time
+import random
+
+def take_snapshot():
+    videoCaptureObject  = cv2.videoCapture(0)
+    result = True
+    while(result):
+        ret,frame = videoCaptureObject.read()
+        cv2.imwrite("NewPicture1.jpg",frame)
+        result = False
+    
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+
+take_snapshot()
